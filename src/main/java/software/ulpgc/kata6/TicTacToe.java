2f51a5cb3a1d4b77c2c5ed7a64f05ae4d808a50c
jjsmx24@gmail.com <jjsmx24@gmail.com>
2023-12-14 18:15:14 +0000

    first commit

--- src/main/java/software/ulpgc/kata6/TicTacToe.java
+++ src/main/java/software/ulpgc/kata6/TicTacToe.java
@@ -1,8 +1,16 @@
 package software.ulpgc.kata6;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import static java.util.Arrays.stream;
+
+
 public class TicTacToe {
-    private static final String cross = "X";
-    private static final String circle = "O";
+    private static final char cross = 'X';
+    private static final char circle = 'O';
     private final String[] board;
 
 
@@ -23,23 +31,42 @@ public class TicTacToe {
 
     private String[] calculateNextSate() {
         String[] result = new String[rows()];
-        for (int i=0; i < rows(); i++){
+        for (int i = 0; i < rows(); i++)
             result[i] = calculateNextState(i);
-        }
         return result;
     }
-
     private String calculateNextState(int row) {
         StringBuilder result = new StringBuilder();
-        for(int i = 0; i < cols(); i++){
-            result.append(calculateNextSate(row, i));
-        }
-        System.out.println(result);
+        for (int i = 0; i < cols(); i++)
+            result.append(calculateNextState(row, i));
         return result.toString();
     }
+    private char calculateNextState(int row, int col) {
+        if(analizewin()){
+            return board[row].charAt(col);
+        }else{
+            return anyRuleFor(row, col) ?  CalculateNextState(row, col) : board[row].charAt(col);
+        }
+    }
 
-    private char calculateNextSate(int row, int col) {
-        return '.';
+    private boolean analizewin() {
+        return (Arrays.toString(board).chars().filter(c -> c == 'X').count() == 3) || (Arrays.toString(board).chars().filter(c -> c == 'O').count() == 3);
+    }
+
+    private char CalculateNextState(int row, int col) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(2);
+        return (randomNumber == 0) ? cross : circle;
+    }
+    private boolean anyRuleFor(int row, int col) {
+        return rules.stream().anyMatch(rule -> rule.check(row, col));
+    }
+    private final List<Rule> rules = List.of(
+            (i, j) -> isEmpty(i,j)
+    );
+
+    private boolean isEmpty(int row, int col) {
+        return board[row].charAt(col) == '.';
     }
 
     private int rows() {
@@ -49,4 +76,10 @@ public class TicTacToe {
     private int cols() {
         return board[0].length();
     }
+
+
+    private interface Rule {
+        boolean check(int row, int col);
+    }
+
 }
--- src/test/java/tests/TicTacToeTests.java
+++ src/test/java/tests/TicTacToeTests.java
@@ -3,6 +3,8 @@ package tests;
 import org.junit.Test;
 import software.ulpgc.kata6.TicTacToe;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.ARRAY;
+
 public class TicTacToeTests {
     @Test
     public void should_return_empty_board_given_empty_board(){
@@ -11,28 +13,46 @@ public class TicTacToeTests {
 
     }
     @Test
-    public void should_return_3x3_empty_board_given_3x3_empty_board(){
+    public void should_return_3x3_completed_board_given_3x3_empty_board(){
         TicTacToe ticTacToe = new TicTacToe("""
                 ...
                 ...
                 ...
                 """.trim()).next();
-        assertThat(ticTacToe.state()).isEqualTo("""
-                ...
-                ...
-                ...
-                """.trim());
+        assertThat(ticTacToe.state().chars().filter(c -> c == '.').count()).isEqualTo(0);
     }
     @Test
-    public void name(){
+    public void should_return_completed_board_with_middle_cross(){
         TicTacToe ticTacToe = new TicTacToe("""
                 ...
-                ...
+                .X.
                 ...
                 """.trim()).next();
+        assertThat(ticTacToe.state().charAt(5)).isEqualTo('X');
+    }
+    @Test
+    public void should_return_cross_horizontal_row_0(){
+        TicTacToe ticTacToe = new TicTacToe(WinCases.cross_horizontal_row_0).next();
+        assertThat(ticTacToe.state()).isEqualTo(WinCases.cross_horizontal_row_0);
+    }
+    @Test
+    public void should_return_circle_horizontal_row_0(){
+        TicTacToe ticTacToe = new TicTacToe(WinCases.circle_horizontal_row_0).next();
+        assertThat(ticTacToe.state()).isEqualTo(WinCases.circle_horizontal_row_0);
     }
 
-    public static class Cases{
+    @Test
+    public void should_return_cross_diagonal_1(){
+        TicTacToe ticTacToe = new TicTacToe(WinCases.cross_diagonal_1).next();
+        assertThat(ticTacToe.state()).isEqualTo(WinCases.cross_diagonal_1);
+    }
+    @Test
+    public void should_return_circle_diagonal_1(){
+        TicTacToe ticTacToe = new TicTacToe(WinCases.circle_diagonal_1).next();
+        assertThat(ticTacToe.state()).isEqualTo(WinCases.circle_diagonal_1);
+    }
+
+    public static class WinCases{
         static final String  cross_horizontal_row_0= """
                 XXX
                 ...
@@ -114,5 +134,4 @@ public class TicTacToeTests {
                 O..
                 """.trim();
     }
-
 }
